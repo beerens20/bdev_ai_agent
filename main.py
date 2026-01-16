5c67f110d8cd88cb084b0e99c8359be17420d191
Mike Beerens <beerens20@gmail.com>
2026-01-16 12:32:29 -0500

    updated main.py and added verbose output mode
    moved the logic and bulk of the code into the main.py function and added a verbose mode to troubleshoot output

--- main.py
+++ main.py
@@ -2,33 +2,37 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="Enter question")
-args = parser.parse_args()
-
-if api_key == None:
-    raise RuntimeError("API Key not found or loaded")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=args.user_prompt,
-)
-# print(
-#     f"Prompt tokens: {response.prompt_token_count}\nResponse tokens: {response.candidates_token_count}"
-# )
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
-print(response.text)
-# testing change
-
 
 def main():
-    print("Hello from bdev-ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+
+    parser.add_argument("user_prompt", type=str, help="Enter question")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    if api_key == None:
+        raise RuntimeError("API Key not found or loaded")
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print(response.text)
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
